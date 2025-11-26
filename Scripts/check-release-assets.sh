56cdd5d9f8d16d8f649538bb46e54e4002ce5550
Peter Steinberger <steipete@gmail.com>
2025-11-26 01:07:38 +0100

    chore(build): align release asset names

--- Scripts/check-release-assets.sh
+++ Scripts/check-release-assets.sh
@@ -5,6 +5,6 @@ ROOT=$(cd "$(dirname "$0")/.." && pwd)
 source "$HOME/Projects/agent-scripts/release/sparkle_lib.sh"
 
 TAG=${1:-$(git describe --tags --abbrev=0)}
-ARTIFACT_PREFIX="RepoBar-"
+ARTIFACT_PREFIX="CodexBar-"
 
 check_assets "$TAG" "$ARTIFACT_PREFIX"
